1cdf01d4b4f20f36086075a6a834f2081245dcd3
precious112 <61796861+precious112@users.noreply.github.com>
2021-01-05 22:01:52 +0100

    Update models.py

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -9,12 +9,4 @@ class Profile(models.Model):
 	def __str__(self):
 		return f'{self.user.username} Profile'
 	
-	def save(self):
-		super().save()
-		
-		img= Image.open(self.image.path)
-		
-		if img.height > 300 and img.width > 300:
-			output_size= (300,300)
-			img.thumbnail(output_size)
-			img.save(self.image.path)
+	
